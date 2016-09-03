448abf68f1e10bbc1cfe7a4099fc0fb939c48cba
Martin Rehder <apple01@rehsco.com>
2016-09-03 08:28:08 +0200

    Initial Commit

---
+++ StyledLabel/StyledLabel.h
@@ -0,0 +1,19 @@
+//
+//  StyledLabel.h
+//  StyledLabel
+//
+//  Created by Martin Rehder on 03.09.16.
+//  Copyright © 2016 Martin Jacob Rehder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StyledLabel.
+FOUNDATION_EXPORT double StyledLabelVersionNumber;
+
+//! Project version string for StyledLabel.
+FOUNDATION_EXPORT const unsigned char StyledLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StyledLabel/PublicHeader.h>
+
+
